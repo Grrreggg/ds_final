df3222569160dae6effc64f5234816e3d663f484
Григорий <3doknite@gmail.com>
2022-04-24 16:09:33 +0700

    Add neural network files and remove useless code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
+
 import sys
+import tensorflow as tf
+
+from tensorflow import keras
 
 if __name__ == "__main__":
     print('args: '+str(sys.argv))
+    tf.keras.models.load_model('dnn_model')
 
     if len(sys.argv) == 1:
         print('NO XLS PATH PROVIDED')
